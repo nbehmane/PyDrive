667c093c86c8e3bef5e446ed5fe84a24a5124026
EE IT Helpdesk <ee-helpdesk@calpoly.edu>
2021-08-26 10:39:10 -0700

    minor changes

--- client.py
+++ client.py
@@ -3,19 +3,7 @@ import psutil
 import json
 import time
 from local import LOCAL_CREDENTIALS
-
-def get_size(bytes, suffix="B"):
-    """
-    Scale bytes to its proper format
-    e.g:
-        1253656 => '1.20MB'
-        1253656678 => '1.17GB'
-    """
-    factor = 1024
-    for unit in ["", "K", "M", "G", "T", "P"]:
-        if bytes < factor:
-            return f"{bytes:.2f}{unit}{suffix}"
-        bytes /= factor
+from funcs import get_size
 
 count = 0
 def tryConnect(server, count):
