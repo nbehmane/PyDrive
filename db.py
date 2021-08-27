4d777c83d6448d32ff52c64db69aa7561ab5e5da
nbehmane <nbehmane@calpoly.edu>
2021-08-27 13:50:17 -0700

    Added Excel support in db.py.

--- db.py
+++ db.py
@@ -23,6 +23,36 @@ def deleteAllEntries():
     col = db["computers"]
     col.remove({})
 
+def importComputerData():
+    client = MongoClient()
+    db = client.computers
+    col = db["computers"]
 
-def dumpExcel():
-    pass
+    wb = openpyxl.Workbook()
+    sheet = wb.active
+    sheet['A1'] = "Computer"
+    sheet['B1'] = "MAC"
+    sheet['C1'] = "Drive"
+    sheet['D1'] = "Total Space"
+    sheet['E1'] = "Total Used"
+    sheet['F1'] = "Total Free"
+    sheet['G1'] = "Time"
+
+    computers = col.find({})
+    count = 2
+    for comp in computers:
+        sheet[f'A{count}'] = comp["Computer"]
+        sheet[f'B{count}'] = comp["MAC"]
+        sheet[f'C{count}'] = comp["Drive"]
+        sheet[f'D{count}'] = comp["Total Space"]
+        sheet[f'E{count}'] = comp["Total Used"]
+        sheet[f'F{count}'] = comp["Total Free"]
+        sheet[f'G{count}'] = comp["Time"]
+        count += 1
+    wb.save(filename="comps.xlsx")
+
+def main():
+    importComputerData()
+
+if __name__ == '__main__':
+    main()
